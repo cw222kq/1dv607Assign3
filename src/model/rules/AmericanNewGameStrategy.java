fc973837ceb88aaa11d1ee34a9e85d22a88eba8c
Charlotte Westerberg <cw222kq@student.lnu.se>
2018-10-02 23:26:09 +0200

    downloading the code to the assignment. Start to modify the code tomorrow

---
+++ src/model/rules/AmericanNewGameStrategy.java
@@ -0,0 +1,31 @@
+package model.rules;
+
+import model.Deck;
+import model.Dealer;
+import model.Player;
+import model.Card;  
+
+class AmericanNewGameStrategy implements INewGameStrategy {
+
+  public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
+    Card c;
+
+    c = a_deck.GetCard();
+    c.Show(true);
+    a_player.DealCard(c);
+
+    c = a_deck.GetCard();
+    c.Show(true);
+    a_dealer.DealCard(c);
+
+    c = a_deck.GetCard();
+    c.Show(true);
+    a_player.DealCard(c);
+
+    c = a_deck.GetCard();
+    c.Show(false);
+    a_dealer.DealCard(c);
+
+    return true;
+  }
+}
